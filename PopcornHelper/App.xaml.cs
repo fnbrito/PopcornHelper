105452b836ede3124f625b69c9e015617108707f
Filipe Brito <madfilipe@hotmail.com>
2023-07-16 18:02:05 -0400

    Add project files.

---
+++ PopcornHelper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PopcornHelper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PopcornHelper/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PopcornHelper
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
